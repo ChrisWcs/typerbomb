1d0fbf67344571f4480e42b3cd6f6ee8dd1db7a9
Christian <ChrisWcs@users.noreply.github.com>
2017-10-05 13:32:37 -0600

    Begin planning App and adding components

---
+++ src/components/typerArea/TyperArea.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class TyperArea extends Component {
+    construtor(props){
+        super(props);
+
+        this.state = {
+            word: "",
+        }
+    }
+
+
+    render(){
+        return(
+            
+        );
+    }
+}
